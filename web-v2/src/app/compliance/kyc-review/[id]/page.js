fc4737d6985ca8e5d5f393634a08b17493ddee96
manzi leila <manzileila03@gmail.com>
2026-05-14 11:50:01 +0200

    Remove frontend from backend repo (managed in separate repo)

--- web-v2/src/app/compliance/kyc-review/[id]/page.js
+++
@@ -1,8 +0,0 @@
-'use client';
-import { useParams, useRouter } from 'next/navigation';
-import AdminKycReviewPage from '../../../(admin)/kyc/[id]/page';
-
-// Compliance officers use the same KYC review UI as admins
-export default function ComplianceKycReviewPage() {
-  return <AdminKycReviewPage />;
-}
